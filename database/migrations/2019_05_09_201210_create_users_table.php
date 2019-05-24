da2789d49da958c6c971a64195caab137ef03c4c
Lavinia Melo <lavininhamelo@gmail.com>
2019-05-24 18:53:03 -0400

    Alteraçao do Migrate

--- database/migrations/2019_05_09_201210_create_users_table.php
+++ database/migrations/2019_05_09_201210_create_users_table.php
@@ -15,17 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('profile_autobridge_id')
+            $table->integer('profile_autobridge_id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('CPF');
             $table->string('CEP');
             $table->string('CNH');
-            $table->string('CNH');
             $table->rememberToken();
-            $table->timestamps();        
-
+            $table->timestamps();
             $table->foreign('profile_autobridge_id')->references('id')->on('profiles')->onDelete('cascade');
 
         });
