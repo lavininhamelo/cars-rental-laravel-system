9dfd1d44789fef020f9a4fa6cdfb41c64d420f6f
Lavinia Melo <lavininhamelo@gmail.com>
2019-05-30 01:37:46 -0400

    Contrução de Views do Dashbord
    Toda a base do painel de controle(Dashbord) administrativo foi montado.

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('titulo','Teste | Autobridge')
+@section('conteudo')
+@include('layouts.boxes')
+@include('layouts.chart')
+
+<div class="container-fluid">
+
+
+  </div>
+@endsection
+
