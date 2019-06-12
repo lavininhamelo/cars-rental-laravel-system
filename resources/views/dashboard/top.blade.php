ccb9d720e1cdcda238a46f78bc62a08dfec85ee8
Lavinia Melo <lavininhamelo@gmail.com>
2019-06-12 16:26:49 -0400

    Correção dos Itens do Dashboard

--- resources/views/dashboard/top.blade.php
+++ resources/views/dashboard/top.blade.php
@@ -74,51 +74,16 @@
         <!--sidebar-menu-->
         <div id="sidebar"><a href="#" class="visible-phone"><i class="icon icon-home"></i> Dashboard</a>
           <ul>
-            <li><a href="#"><i class="icon icon-home"></i> <span>Dashboard</span></a> </li>
-            <li> <a href="#"><i class="icon icon-signal"></i> <span>Charts &amp; graphs</span></a> </li>
-            <li> <a href="#"><i class="icon icon-inbox"></i> <span>Widgets</span></a> </li>
-            <li><a href="#"><i class="icon icon-th"></i> <span>Tables</span></a></li>
-            <li><a href="#"><i class="icon icon-fullscreen"></i> <span>Full width</span></a></li>
-            <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Forms</span> <span class="label label-important">3</span></a>
-              <ul>
-                <li><a href="#">Basic Form</a></li>
-                <li><a href="#">Form with Validation</a></li>
-                <li><a href="#">Form with Wizard</a></li>
-              </ul>
-            </li>
-            <li><a href="#"><i class="icon icon-tint"></i> <span>Buttons &amp; icons</span></a></li>
-            <li><a href="#"><i class="icon icon-pencil"></i> <span>Eelements</span></a></li>
-            <li class="submenu"> <a href="#"><i class="icon icon-file"></i> <span>Addons</span> <span class="label label-important">5</span></a>
-              <ul>
-                <li><a href="#">Dashboard2</a></li>
-                <li><a href="#">Gallery</a></li>
-                <li><a href="#">Calendar</a></li>
-                <li><a href="#">Invoice</a></li>
-                <li><a href="#">Chat option</a></li>
-              </ul>
-            </li>
-            <li class="submenu"> <a href="#"><i class="icon icon-info-sign"></i> <span>Error</span> <span class="label label-important">4</span></a>
-              <ul>
-                <li><a href="#">Error 403</a></li>
-                <li><a href="#">Error 404</a></li>
-                <li><a href="#">Error 405</a></li>
-                <li><a href="#">Error 500</a></li>
-              </ul>
-            </li>
-            <li class="content"> <span>Monthly Bandwidth Transfer</span>
-              <div class="progress progress-mini progress-danger active progress-striped">
-                <div style="width: 77%;" class="bar"></div>
-              </div>
-              <span class="percent">77%</span>
-              <div class="stat">21419.94 / 14000 MB</div>
-            </li>
-            <li class="content"> <span>Disk Space Usage</span>
-              <div class="progress progress-mini active progress-striped">
-                <div style="width: 87%;" class="bar"></div>
-              </div>
-              <span class="percent">87%</span>
-              <div class="stat">604.44 / 4000 MB</div>
-            </li>
+            <li><a href="#"><i class="icon icon-home"></i>      <span>Dashboard</span></a> </li>
+            <li><a href="#"><i class="icon icon-signal"></i>   <span>Aluguéis</span></a> </li>
+            <li><a href="/vehicles"><i class="icon icon-road"></i>    <span>Veículos</span></a> </li>
+            <li><a href="#"><i class="icon icon-user"></i>        <span>Usuários</span></a></li>
+            <li><a href="#"><i class="icon icon-cog"></i><span>Manutenção</span></a></li>
+            <li><a href="#"><i class="icon icon-tint"></i>      <span>Agências</span></a></li>
+            <li><a href="#"><i class="icon icon-th"></i>    <span>Funcionários</span></a></li>
+
+
+
           </ul>
         </div>
         <!--sidebar-menu-->
