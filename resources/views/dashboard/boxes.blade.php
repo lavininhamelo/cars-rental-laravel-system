98debdfe8b80a0cd24b7c1a424ae15e8c52a172f
Lavinia Melo <lavininhamelo@gmail.com>
2019-06-29 04:50:18 -0400

    Correções simples

--- resources/views/dashboard/boxes.blade.php
+++ resources/views/dashboard/boxes.blade.php
@@ -1,10 +1,10 @@
 <!--Action boxes-->
     <div class="quick-actions_homepage">
       <ul class="quick-actions">
-        <li class="bg_ls"> <a href="#"> <i class="icon-dashboard"></i> <span class="label label-important">7</span> Meu Dashboard </a> </li>
+        <li class="bg_ls"> <a href="/home"> <i class="icon-dashboard"></i> <span class="label label-important">7</span> Meu Dashboard </a> </li>
         <li class="bg_lb"> <a href="/vehicles"> <i class="icon-road"></i>Veiculos</a> </li>
-        <li class="bg_lg"> <a href="#"> <i class="icon-money"></i>Aluguéis</a> </li>
-        <li class="bg_ly"> <a href="#"> <i class="icon-cog"></i><span class="label label-success">1</span>Manutenção</a> </li>
+        <li class="bg_lg"> <a href="/rentals"> <i class="icon-money"></i>Aluguéis</a> </li>
+        <li class="bg_ly"> <a href="/rentalagency"> <i class="icon-th"></i><span class="label label-success">1</span>Agências</a> </li>
         <li class="bg_lo"> <a href="/users"> <i class="icon-user"></i>Usuários</a> </li>
         <li class="bg_lr"> <a href="#"> <i class="icon-info-sign"></i> Avisos</a> </li>
 
